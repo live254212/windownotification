dd67f7d4b0670c7afe9f6e0fa49f688e3fa0f44e
Livewire <livewirepalayamkotai@gmail.com>
2022-03-07 10:23:53 +0530

    Python Notification

---
+++ sample.py
@@ -0,0 +1,16 @@
+"""from tkinter import*
+t=Tk()
+Checkbutton(t, text='male').grid(row=0, sticky=W) 
+
+Checkbutton(t, text='female').grid(row=1, sticky=W) """
+from tkinter import *
+master = Tk() 
+Label(master, text='First Name').grid(row=0) 
+Label(master, text='Last Name').grid(row=1) 
+e1 = Entry(master) 
+e2 = Entry(master) 
+e1.grid(row=0, column=1) 
+e2.grid(row=1, column=1) 
+mainloop() 
+
+
